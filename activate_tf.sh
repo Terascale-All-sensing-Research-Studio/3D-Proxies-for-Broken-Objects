facaab879b7c3137f85881ae254abcbbd593d85c
nikwl <nikolas.lamb@gmail.com>
2021-11-29 11:55:35 -0500

    site rebasing

--- activate_tf.sh
+++
@@ -1,4 +0,0 @@
-# script that activates the tensorflow environment
-source env-tf/bin/activate
-source constants.sh
-echo "The primary data directory is located at: "$DATADIR
--- activate_torch.sh
+++
@@ -1,4 +0,0 @@
-# script that activates the torch environment
-source env-torch/bin/activate
-source constants.sh
-echo "The primary data directory is located at: "$DATADIR
